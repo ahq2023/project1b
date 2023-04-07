120fc6c8c045177ac31eaaef80e40cf0ac894d76
ahq2023 <hxqin682@connect.hkust-gz.edu.cn>
2023-04-07 18:25:46 +0800

    Upload

---
+++ data.js
@@ -0,0 +1,650 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-interior_views_a_corridor_with_only_closed_doors_f",
+      "name": "interior_views_a_corridor_with_only_closed_doors_f",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0.11258414826791707,
+        "pitch": -1.2067410138921506,
+        "fov": 1.3360510069328058
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.038143636788214,
+          "pitch": 0.08410514291980853,
+          "rotation": 0,
+          "target": "1-advanced_no_style_words_5_people_in_black_standi"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.35317789951996126,
+          "pitch": -1.2414469171855913,
+          "title": "You wake up in a corridor.",
+          "text": "There are doors everywhere. You are alone."
+        },
+        {
+          "yaw": 1.2236695001841031,
+          "pitch": 0.03271518796908879,
+          "title": "Door locked.",
+          "text": "Try another one."
+        },
+        {
+          "yaw": -0.7428039047521686,
+          "pitch": 0.07040933399516192,
+          "title": "Door locked.",
+          "text": "Try another one."
+        },
+        {
+          "yaw": -1.3494051863459848,
+          "pitch": 0.03993290355267831,
+          "title": "Door locked.",
+          "text": "Try another one."
+        },
+        {
+          "yaw": -1.850624596780845,
+          "pitch": 0.27553029825806874,
+          "title": "Door locked.",
+          "text": "Try another one."
+        },
+        {
+          "yaw": -0.41192604421608436,
+          "pitch": 0.1076898429987434,
+          "title": "Door locked.",
+          "text": "Try another one.<br>"
+        },
+        {
+          "yaw": 1.4498496023973635,
+          "pitch": 0.14313911298776816,
+          "title": "Door locked.",
+          "text": "Try another one."
+        }
+      ]
+    },
+    {
+      "id": "1-advanced_no_style_words_5_people_in_black_standi",
+      "name": "advanced_no_style_words_5_people_in_black_standi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 1.7097160567374452,
+        "pitch": -0.21565487618296153,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3869825522017649,
+          "pitch": 0.11898754242827536,
+          "rotation": 0,
+          "target": "2-infrared_experimental_nature_scenes_black-and-wh"
+        },
+        {
+          "yaw": -0.032882648295387895,
+          "pitch": 0.13768786334518524,
+          "rotation": 0,
+          "target": "3-realistic_angry_faceless_man_in_dark_coat_standing"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4319424051680762,
+          "pitch": -0.005535421629808468,
+          "title": "You can't go back.",
+          "text": "The door back is locked."
+        },
+        {
+          "yaw": 2.8211983704906096,
+          "pitch": -0.10702912410570953,
+          "title": "EXIT",
+          "text": "Wrong way. The stairs to the top of the tower are on the other side."
+        },
+        {
+          "yaw": 1.65102717391186,
+          "pitch": 0.09575005061186204,
+          "title": "Climb the stairs on the left.",
+          "text": "Everyone is climbing to the top of the tower. Why shouldn't you?"
+        },
+        {
+          "yaw": -1.632837093178118,
+          "pitch": 0.08947656483435651,
+          "title": "Climb the stairs on your right.",
+          "text": "Everyone is climbing."
+        },
+        {
+          "yaw": 3.0774417342629636,
+          "pitch": 0.15286070578233435,
+          "title": "You shouldn't be going out there.",
+          "text": "There is nothing and no one out there. You'd live and die alone."
+        },
+        {
+          "yaw": -2.8834805224746844,
+          "pitch": -0.0518332675081119,
+          "title": "There's nothing out there.",
+          "text": "Stay in the tower. The outside is a desert. These majestic silhouettes you see are only figments of your delusions."
+        },
+        {
+          "yaw": 0.734979478093317,
+          "pitch": 0.19180345076530614,
+          "title": "Prove your worth by climbing to the top.",
+          "text": "You don't want to live your whole life at the bottom of everyone else, right?"
+        },
+        {
+          "yaw": -2.7108898648488964,
+          "pitch": 0.18596479347408135,
+          "title": "You want to be yourself?",
+          "text": "Try to be part of society first."
+        },
+        {
+          "yaw": -0.26704621991554056,
+          "pitch": -0.008286826420537352,
+          "title": "The stairs are crowded.",
+          "text": "People a blocking your way. As you try to walk forward, a man stops you, telling you to wait behind him."
+        },
+        {
+          "yaw": -0.42786675417863407,
+          "pitch": 0.05628618464028001,
+          "title": "WAIT BEHIND THE MAN",
+          "text": ""
+        },
+        {
+          "yaw": -0.004092713001799808,
+          "pitch": 0.06819882187579651,
+          "title": "IGNORE THE MAN AND CLIMB!",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-infrared_experimental_nature_scenes_black-and-wh",
+      "name": "infrared_experimental_nature_scenes_black-and-wh",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": -0.6371389228131292,
+        "pitch": 0.006648294298363666,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9033832796111625,
+          "pitch": 0.046621670413740546,
+          "rotation": 0,
+          "target": "0-interior_views_a_corridor_with_only_closed_doors_f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8939931631172069,
+          "pitch": -0.1925338238066736,
+          "title": "You wait behind the man.",
+          "text": "You wait and wait and never reach the top of the tower. You grow old and fall into a deep slumber one day."
+        }
+      ]
+    },
+    {
+      "id": "3-realistic_angry_faceless_man_in_dark_coat_standing",
+      "name": "realistic_angry_faceless_man_in_dark_coat_standing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 1.6295094276053055,
+        "pitch": -0.20250398625718624,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3362882375355696,
+          "pitch": 0.29896979513393696,
+          "rotation": 0,
+          "target": "4-nebula_red_moon_staircase_in_fire_with_a_person_st"
+        },
+        {
+          "yaw": -1.6262917901590388,
+          "pitch": 0.02459943899371453,
+          "rotation": 0,
+          "target": "2-infrared_experimental_nature_scenes_black-and-wh"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3329632930032904,
+          "pitch": -0.46731511913328916,
+          "title": "The man blocks your way.",
+          "text": "<font color=\"#eeeeee\" face=\"Helmet, Freesans, sans-serif\"><span style=\"font-size: 16px; background-color: rgb(17, 17, 17);\">\"Hey! Didn't you hear me? Stay in the line. First come, first served.\"</span></font>"
+        },
+        {
+          "yaw": 1.3436143940904213,
+          "pitch": 0.17576156298988543,
+          "title": "PUNCH THE MAN",
+          "text": ""
+        },
+        {
+          "yaw": -1.6111102447916537,
+          "pitch": -0.12587112573727666,
+          "title": "STAY IN THE LINE",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "4-nebula_red_moon_staircase_in_fire_with_a_person_st",
+      "name": "nebula_red_moon_staircase_in_fire_with_a_person_st",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0.3606104231380449,
+        "pitch": -0.049346321730224574,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1644352648860696,
+          "pitch": 0.21927005349515127,
+          "rotation": 0,
+          "target": "5-dreamlike_red_moon_staircase"
+        },
+        {
+          "yaw": -1.7326733058207715,
+          "pitch": 0.3004763860621704,
+          "rotation": 0,
+          "target": "2-infrared_experimental_nature_scenes_black-and-wh"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.06196749637823906,
+          "pitch": 0.1633780754661558,
+          "title": "You punched the man to death.",
+          "text": "You've now moved forward."
+        },
+        {
+          "yaw": -1.9121569253143473,
+          "pitch": -0.04249330889857106,
+          "title": "Someone else is blocking your path.",
+          "text": ""
+        },
+        {
+          "yaw": -2.1408396313317652,
+          "pitch": 0.13003356497763008,
+          "title": "GET RID OF THEM.",
+          "text": ""
+        },
+        {
+          "yaw": -1.8067379091572793,
+          "pitch": 0.1980808303042796,
+          "title": "WAIT BEHIND THEM.",
+          "text": "You've done enough damage."
+        }
+      ]
+    },
+    {
+      "id": "5-dreamlike_red_moon_staircase",
+      "name": "dreamlike_red_moon_staircase",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4520413448681673,
+          "pitch": -0.104076188853373,
+          "rotation": 0,
+          "target": "6-dreamlike_white_room_with_nothing"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.4130043418413365,
+          "pitch": 0.055515154526736055,
+          "title": "You punch and push your way through.",
+          "text": "People keep challenging you, and you keep fighting."
+        },
+        {
+          "yaw": 0.9800265589051484,
+          "pitch": 0.08550990154719784,
+          "title": "Once, you almost got pushed off the edge.",
+          "text": "Someone grabbed your hand and saved you."
+        },
+        {
+          "yaw": 1.8672654590879612,
+          "pitch": -0.07524379494934408,
+          "title": "\"Let's climb the tower together.\"",
+          "text": "\"Yes, we'll be stronger together.\""
+        }
+      ]
+    },
+    {
+      "id": "6-dreamlike_white_room_with_nothing",
+      "name": "dreamlike_white_room_with_nothing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 2.3935585881201806,
+        "pitch": 0.07275673593968612,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6362400311666292,
+          "pitch": 0.4065399740042288,
+          "rotation": 0,
+          "target": "8-interior_views_empty_white_room-1"
+        },
+        {
+          "yaw": -1.2176455224118676,
+          "pitch": 0.42881042174679074,
+          "rotation": 0,
+          "target": "7-nebula_red_moon_staircase_in_fire"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.218849181306707,
+          "pitch": 0.04322983276138004,
+          "title": "You and your friend climb higher and higher.",
+          "text": "Having each other's back made the journey slightly easier."
+        },
+        {
+          "yaw": -2.2702721548291294,
+          "pitch": -0.05492574802810424,
+          "title": "One day...",
+          "text": "You finally find yourselves near the top of the world."
+        },
+        {
+          "yaw": -1.3315279388148422,
+          "pitch": -0.10623637664782137,
+          "title": "There is a room at the end of the road.",
+          "text": "It must be the legendary top of the tower that everyone has been reaching for."
+        },
+        {
+          "yaw": -1.275211436707945,
+          "pitch": 0.12318271155431404,
+          "title": "<span style=\"font-size: 13px; background-color: rgba(58, 68, 84, 0.8);\">\"ONE PERSON AT A TIME,\" a board says.</span>",
+          "text": "It seems that the room only allows one person."
+        },
+        {
+          "yaw": -1.5812840552478775,
+          "pitch": 0.318929319523388,
+          "title": "PUSH YOUR FRIEND OFF",
+          "text": ""
+        },
+        {
+          "yaw": -1.3025756979761347,
+          "pitch": 0.386845216660614,
+          "title": "YOU CAN'T HURT YOUR FRIEND.",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "7-nebula_red_moon_staircase_in_fire",
+      "name": "nebula_red_moon_staircase_in_fire",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0.3642021121563168,
+        "pitch": 0.22378659392626687,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.21026428686738008,
+          "pitch": 0.41873794801209385,
+          "rotation": 0,
+          "target": "0-interior_views_a_corridor_with_only_closed_doors_f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.10005767174234848,
+          "pitch": 0.17592872167168494,
+          "title": "You couldn't betray your friend...",
+          "text": "... so they betrayed you first. They push you off the tower. You fall into suffocating flames and black out."
+        },
+        {
+          "yaw": 2.2260019535534035,
+          "pitch": -0.2883723833610006,
+          "title": "Why would your friend do this?",
+          "text": "When you couldn't betray them..."
+        },
+        {
+          "yaw": -2.4133618646257595,
+          "pitch": 0.4074752707178,
+          "title": "Would it have been better if you betrayed them first?",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "8-interior_views_empty_white_room-1",
+      "name": "interior_views_empty_white_room (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": -2.460054508692327,
+        "pitch": 0.1342507361444767,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.33359362530234016,
+          "pitch": 0.1897008409673795,
+          "rotation": 0,
+          "target": "0-interior_views_a_corridor_with_only_closed_doors_f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.042410655044714,
+          "pitch": -0.04094814171906336,
+          "title": "You pushed your friend off the tower...",
+          "text": "... and got into the coveted top room. Congratulations! You're now at the top of the world, the owner for the throne every is fighting for: a very empty room."
+        },
+        {
+          "yaw": -1.0619500039523082,
+          "pitch": 0.13628481622033206,
+          "title": "There is nothing else in the room...",
+          "text": "No sign of life... only silence and loneliness."
+        },
+        {
+          "yaw": -0.25528837081472133,
+          "pitch": -0.007004437399276142,
+          "title": "Is this what you're been fighting for?",
+          "text": "Was it worth it?"
+        },
+        {
+          "yaw": 2.690545896173253,
+          "pitch": 0.09185299060025365,
+          "title": "What have you done?",
+          "text": "And for what end?"
+        },
+        {
+          "yaw": 1.0971755841060578,
+          "pitch": 0.07167480114166125,
+          "title": "It's so empty up here...",
+          "text": "What is the point of spilling all those blood, sweat, and tears to come up here?"
+        },
+        {
+          "yaw": 0.3320450997336444,
+          "pitch": 0.01247189110655711,
+          "title": "Would it have been better otherwise?",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Project1b",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
